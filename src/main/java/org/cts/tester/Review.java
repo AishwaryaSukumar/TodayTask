a80db8ef1321663091829b6569c808c81c067819
registered <registered>
2019-07-11 19:05:03 +0530

    adding code

---
+++ src/main/java/org/cts/tester/Review.java
@@ -0,0 +1,27 @@
+package org.cts.tester;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Review {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Dr.Kamaraj\\eclipse-workspace\\ReadWrite\\div\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		WebElement w1 = driver.findElement(By.name("proceed"));
+		w1.click();
+		Alert al=driver.switchTo().alert();
+		al.accept();
+		
+		
+	}
+
+}
+
+
+
+
+
